010d19791ae8d6f495d8494674d93bc18ebf9095
GabrielCanarte14 <lcanarte@espol.edu.ec>
2023-08-13 17:33:13 -0500

    material

---
+++ src/app/pages/about/about.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent {
+  description: string = 'Esta es la descripcion del proyecto 7 de la materia de Desarrollo de Aplicaciones Web y Moviles'
+}
